d3714218e4bef0bf88498adcb24fcc5df841901d
Valery <jim_west_93@icloud.com>
2021-11-01 06:52:21 +0300

    final commit

--- DogsTestApp/Network/DogsAPIManager.swift
+++ DogsTestApp/Network/DogsAPIManager.swift
@@ -24,7 +24,7 @@ class DogsAPIManager {
         
         URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
             
-            DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 1) {
+            DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
                 
                 if let error = error {
                     print(error)
@@ -97,36 +97,32 @@ class DogsAPIManager {
     
     func fetchPhoto(urlString: String, complition: @escaping (UIImage) -> ()) {
         
-        DispatchQueue.global(qos: .background).async {
-            
-            if let imageFromCache = self.imageCache.object(forKey: urlString as NSString) {
-                DispatchQueue.main.async {
-                    complition(imageFromCache)
-                }
-            } else {
-                
-                guard let url = URL(string: urlString) else { return }
-                
-                let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 5)
-                let dataTask = URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
-                    
-                    if let error = error {
-                        print(error)
-                    }
-                    
-                    guard let data = data else { return }
-                    
-                    guard let image = UIImage(data: data) else { return }
-                    
-                    DispatchQueue.main.async {
-                        complition(image)
-                    }
-                    self?.imageCache.setObject(image, forKey: urlString as NSString)
-                }
-                
-                dataTask.resume()
-                
+        if let imageFromCache = self.imageCache.object(forKey: urlString as NSString) {
+            DispatchQueue.main.async {
+                complition(imageFromCache)
             }
+        } else {
+            
+            guard let url = URL(string: urlString) else { return }
+            
+            let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 5)
+            let dataTask = URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
+                
+                if let error = error {
+                    print(error)
+                }
+                
+                guard let data = data else { return }
+                
+                guard let image = UIImage(data: data) else { return }
+                
+                DispatchQueue.main.async {
+                    complition(image)
+                }
+                self?.imageCache.setObject(image, forKey: urlString as NSString)
+            }
+            
+            dataTask.resume()
             
         }
         
